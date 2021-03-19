259fc3d9762357f15fafcd2be47e3b338af642e3
aniledev <elinamcgill@gmail.com>
2021-03-19 01:31:48 -0400

    REFRACTOR: update jsx with component generation method

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -55,23 +55,7 @@ class DashboardRoute extends Component {
         <p>Total correct answers: 7</p>
         <h3>Words to Practice</h3>
         <div className="words-to-practice">
-          <ul>
-            <li>
-              <p>Words</p>
-              <p>correct answer: 3</p>
-              <p>incorrect answer 3</p>
-            </li>
-            <li>
-              <p>Words</p>
-              <p>correct answer: 3</p>
-              <p>incorrect answer 3</p>
-            </li>
-            <li>
-              <p>Words</p>
-              <p>correct answer: 3</p>
-              <p>incorrect answer 3</p>
-            </li>
-          </ul>
+          {this.context.words ? this.wordListInfo(this.context.words) : null}
         </div>
         <Link className="start-practicing-link" to="/learn">
           <Button>Start Practicing</Button>
