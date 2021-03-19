ddb53af5566324dfa607321f1b23025665100ad0
aniledev <elinamcgill@gmail.com>
2021-03-18 23:27:05 -0400

    FEAT: flesh out Dashboard route and placeholder JSX according to cypress tests and wireframes

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class DashboardRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        {/* h2 and p need to be updated with info from the server*/}
+        <h2 className="dashboard-header">Test language 1</h2>
+        <p>Total correct answers: 7</p>
+        <h3>Words to Practice</h3>
+        <div className="words-to-practice">
+          <ul>
+            <li>Word</li>
+            <li>Word</li>
+            <li>Word</li>
+            <li>Word</li>
+            <li>Word</li>
+          </ul>
+        </div>
+        <Link className="start-practicing-link" to="/learn">
+          Start Practicing
+        </Link>
       </section>
     );
   }
 }
 
-export default DashboardRoute
+export default DashboardRoute;
