428d398e2381e290e7a73a543670852563913187
aniledev <elinamcgill@gmail.com>
2021-03-19 16:26:51 -0400

    CHORE: remove unnecessary import statement

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
 import App from "../components/App/App";
 
 it("renders without crashing", () => {
