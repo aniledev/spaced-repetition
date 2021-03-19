ea8a328f1c51e9d224110355c606cb6c47dfd62a
aniledev <elinamcgill@gmail.com>
2021-03-19 10:43:16 -0400

    FEAT: write dummy JAX elements based on results from cypress test

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -3,8 +3,40 @@ import React, { Component } from 'react'
 class LearningRoute extends Component {
   render() {
     return (
-      <section>
-        implement and style me
+      <section className="learn-route">
+        {/* update p, h3, and last 2 p with data from the server once the component mounts */}
+        <p>Your total score is: 999</p>
+        <h2 className="learn-route-header">Translate the word:</h2>
+        <span>
+          <h3>Testnextword</h3>
+        </span>
+
+        <form onSubmit={this.handleSubmit}>
+          <Label htmlFor="learn-guess-input">
+            What's the translation for this word?
+            <Required />
+          </Label>
+          <Input
+            className="learn-guess-input"
+            ref={this.firstInput}
+            id="learn-guess-input"
+            name="guess"
+            type="text"
+            placeholder="translation"
+            maxLength="20"
+            autofocus={true}
+            required
+          />
+          <Button className="guess-word-button" type="submit">
+            Submit Answer
+          </Button>
+        </form>
+        <p className="word-count-stats">
+          You have answered this word correctly 222 times.
+        </p>
+        <p className="word-count-stats">
+          You have answered this word incorrectly 333 times.
+        </p>
       </section>
     );
   }
