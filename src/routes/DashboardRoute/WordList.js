8b6941df87b9d7ee24d370455f4fce85422a1386
aniledev <elinamcgill@gmail.com>
2021-03-19 01:47:26 -0400

    REFRACTOR: add classNames to JSX elements for future styling

--- src/routes/DashboardRoute/WordList.js
+++ src/routes/DashboardRoute/WordList.js
@@ -4,8 +4,10 @@ export default function WordList(props) {
   return (
     <li>
       <h4>{props.word.original}</h4>
-      <p>Correct answer: {props.word.correct_count}</p>
-      <p>Incorrect answer: {props.word.incorrect_count}</p>
+      <p className="word-info">Correct answer: {props.word.correct_count}</p>
+      <p className="word-info">
+        Incorrect answer: {props.word.incorrect_count}
+      </p>
     </li>
   );
 }
