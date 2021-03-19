c80118a6438d42bc7130bc1297d0916ccbeea02a
aniledev <elinamcgill@gmail.com>
2021-03-19 01:30:47 -0400

    FEAT: create simple wordlist component with dummy information

---
+++ src/routes/DashboardRoute/WordList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function WordList(props) {
+  return (
+    <li>
+      <p>Words</p>
+      <p>correct answer: 4</p>
+      <p>incorrect answer 3</p>
+    </li>
+  );
+}
