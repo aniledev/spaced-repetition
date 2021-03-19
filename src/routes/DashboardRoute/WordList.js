e1fb290bbbf4a5fb9dd9d35de44c59350b5757c3
aniledev <elinamcgill@gmail.com>
2021-03-19 01:42:50 -0400

    REFRACTOR: update WordList to display words from the props

--- src/routes/DashboardRoute/WordList.js
+++ src/routes/DashboardRoute/WordList.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function WordList(props) {
   return (
     <li>
-      <p>Words</p>
+      <h4>{props.word.original}</h4>
       <p>correct answer: 4</p>
       <p>incorrect answer 3</p>
     </li>
