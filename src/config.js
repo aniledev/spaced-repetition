8136a790246d7b2ec7f411caa76db5faeefe7ecb
aniledev <elinamcgill@gmail.com>
2021-03-19 18:15:29 -0400

    CHORE: update config vars

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  API_ENDPOINT: process.env.REACT_APP_PORT_URL || "https://stormy-meadow-18456.herokuapp.com/",
+  API_ENDPOINT: process.env.REACT_APP_PORT_URL || "https://stormy-meadow-18456.herokuapp.com/api",
   TOKEN_KEY: "blogful-client-auth-token",
 };
