5cc015870847e600e02a476e7482299a6c707ef8
aniledev <elinamcgill@gmail.com>
2021-03-26 14:51:31 -0400

    CHORE: update vercel config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  API_ENDPOINT:
-    process.env.REACT_APP_PORT_URL ||
-    "http://localhost:8000/api",
-  TOKEN_KEY: "blogful-client-auth-token",
+  API_ENDPOINT: process.env.REACT_APP_PORT_URL || "http://localhost:8000/api",
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || "blogful-client-auth-token",
 };
